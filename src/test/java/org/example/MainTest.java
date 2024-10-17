5612c4d58cd3bb3eba2edb8b541d9b68ceba409a
alfo0924 <halo0924@yahoo.com.tw>
2024-10-17 22:43:35 +0800

    v1.015  add test function

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -14,10 +14,26 @@ class MainTest {
         assertEquals(expected, actual);
 
     }
+    @Test
     void b_add(){
         Main m =new Main();
-        int expected =20;
-        int actual =m.add(1,3);
+        int expected = 19;
+        int actual = m.add(1,3);
         assertEquals(expected,actual);
     }
+//    @Test
+    void c_add(){
+        Main m2=new Main();
+        int expected =25;
+        int actual =m2.add(1,4);
+    }
+    @Test
+    void d_add(){
+        Main m3=new Main();
+        int expected =33;
+        int actual = m3.add(3,6);
+    }
+
+
+
 }
